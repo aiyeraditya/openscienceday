ce4e76524109693843f976df0585a9036971a1d0
Aditya Iyer <37734994+aiyeraditya@users.noreply.github.com>
2025-06-13 16:06:51 +0200

    Support For Multiple People

--- main.js
+++ main.js
@@ -21,18 +21,19 @@ const joints = Array.from({ length: 33 }, () => {
   return sphere;
 });
 
-// Define key bone connections
+// Define key bone connections for MoveNet (17 keypoints)
 const bones = [];
 const boneMaterial = new THREE.LineBasicMaterial({ color: 0x00ff00 });
 
 const CONNECTED_JOINTS = [
-  [11, 13], [13, 15], // Left arm
-  [12, 14], [14, 16], // Right arm
-  [11, 12],           // Shoulders
-  [23, 24],           // Hips
-  [11, 23], [12, 24], // Torso
-  [23, 25], [25, 27], // Left leg
-  [24, 26], [26, 28]  // Right leg
+  [0, 1], [1, 3], [0, 2], [2, 4], // Head to shoulders to elbows to wrists
+  [0, 5], [5, 7], [7, 9],         // Left arm
+  [0, 6], [6, 8], [8, 10],        // Right arm
+  [5, 6],                         // Shoulders
+  [5, 11], [6, 12],               // Torso sides
+  [11, 12],                       // Hips
+  [11, 13], [13, 15],             // Left leg
+  [12, 14], [14, 16]              // Right leg
 ];
 
 CONNECTED_JOINTS.forEach(() => {
@@ -54,44 +55,88 @@ navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
   video.srcObject = stream;
 });
 
-// Pose detection with MediaPipe
-const pose = new Pose({
-  locateFile: file => `https://cdn.jsdelivr.net/npm/@mediapipe/pose/${file}`
-});
+// --- TensorFlow.js and MoveNet MultiPose integration ---
+let model;
 
-pose.setOptions({
-  modelComplexity: 1,
-  smoothLandmarks: true,
-  enableSegmentation: false,
-  selfieMode: true
-});
+async function loadModel() {
+  model = await poseDetection.createDetector(
+    poseDetection.SupportedModels.MoveNet,
+    {
+      modelType: poseDetection.movenet.modelType.MULTIPOSE_LIGHTNING
+    }
+  );
+}
 
-pose.onResults(results => {
-  if (!results.poseLandmarks) return;
+// Helper to create joints and bones for each person
+function createPersonObjects() {
+  const people = [];
+  for (let i = 0; i < 6; i++) { // Up to 6 people
+    const personJoints = Array.from({ length: 17 }, () => {
+      const sphere = new THREE.Mesh(jointGeometry, jointMaterial);
+      sphere.visible = false;
+      scene.add(sphere);
+      return sphere;
+    });
+    const personBones = CONNECTED_JOINTS.map(() => {
+      const geometry = new THREE.BufferGeometry().setFromPoints([new THREE.Vector3(), new THREE.Vector3()]);
+      const line = new THREE.Line(geometry, boneMaterial);
+      line.visible = false;
+      scene.add(line);
+      return line;
+    });
+    people.push({ joints: personJoints, bones: personBones });
+  }
+  return people;
+}
 
-  // Scale landmarks to 3D space
-  results.poseLandmarks.forEach((lm, i) => {
-    const x = (lm.x - 0.5) * window.innerWidth;
-    const y = -(lm.y - 0.5) * window.innerHeight;
-    const z = -lm.z * 100;
-    joints[i].position.set(x, y, z);
-  });
-
-  // Update bones
-  CONNECTED_JOINTS.forEach(([a, b], i) => {
-    const start = joints[a].position;
-    const end = joints[b].position;
-    bones[i].geometry.setFromPoints([start, end]);
-  });
-
-  renderer.render(scene, camera);
-});
+const people = createPersonObjects();
 
 // Process frames
 async function processFrame() {
-  await pose.send({ image: video });
+  if (!model) return requestAnimationFrame(processFrame);
+  const input = tf.browser.fromPixels(video);
+  const predictions = await model.estimatePoses(input, { maxPoses: 6, flipHorizontal: false });
+  input.dispose();
+
+  // Hide all joints/bones by default
+  people.forEach(person => {
+    person.joints.forEach(j => j.visible = false);
+    person.bones.forEach(b => b.visible = false);
+  });
+
+  predictions.forEach((pose, pIdx) => {
+    if (pose.keypoints == null) return;
+    const person = people[pIdx];
+    pose.keypoints.forEach((kp, i) => {
+      if (kp.score > 0.3) {
+        const x = (kp.x - video.width/2);
+        const y = -(kp.y - video.height/2);
+        const z = 0;
+        person.joints[i].position.set(x, y, z);
+        person.joints[i].visible = true;
+      } else {
+        person.joints[i].visible = false;
+      }
+    });
+    CONNECTED_JOINTS.forEach(([a, b], i) => {
+      if (person.joints[a].visible && person.joints[b].visible) {
+        person.bones[i].geometry.setFromPoints([
+          person.joints[a].position,
+          person.joints[b].position
+        ]);
+        person.bones[i].visible = true;
+      } else {
+        person.bones[i].visible = false;
+      }
+    });
+  });
+
+  renderer.render(scene, camera);
   requestAnimationFrame(processFrame);
 }
 
-// Start
-video.onloadeddata = () => processFrame();
+// Start everything
+video.onloadeddata = async () => {
+  await loadModel();
+  processFrame();
+};
